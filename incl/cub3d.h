d0b81ef72abacdb9ff1a1a52dcb9b69dff616e0c
Guillem Prada Torres <134978329+GPradaT@users.noreply.github.com>
2024-04-14 09:01:31 +0200

    continuando en la intr

--- incl/cub3d.h
+++ incl/cub3d.h
@@ -6,17 +6,32 @@
 /*   By: gprada-t <gprada-t@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 10:50:20 by gprada-t          #+#    #+#             */
-/*   Updated: 2024/04/10 14:51:41 by gprada-t         ###   ########.fr       */
+/*   Updated: 2024/04/13 18:34:49 by gprada-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <math.h>
-#include <mlx.h>
-#include <string.h>
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <math.h>
+# include <mlx.h>
+# include <string.h>
+
+# define SUCCESS 0
+# define FAILURE 1
+# define TRUE 1
+# define FALSE 0
+
+
+# define NUM_TEXTURES 4
+# define TILE_SIZE 64
+# define FOV 60
+# define PI 3.14159265359
+
 
 typedef struct s_data
 {
@@ -40,12 +55,25 @@ typedef struct s_map
 	int			cols;
 }				t_map;
 
+typedef struct s_file
+{
+	char		*no;
+	char		*so;
+	char		*we;
+	char		*ea;
+	char		*s;
+	int			f;
+	int			c;
+}				t_file;
 typedef struct s_game
 {
 	t_screen	*screen;
 	t_data		img;
 	t_map		*map;
-	t_player	*player;
-	t_ray		*ray;
-	t_texture	*texture;
+	t_file		*file;
+	void		*player;
+	void		*ray;
+	void		*texture;
 }				t_game;
+
+#endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: gprada-t <gprada-t@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 10:42:40 by gprada-t          #+#    #+#             */
-/*   Updated: 2024/04/10 15:02:19 by gprada-t         ###   ########.fr       */
+/*   Updated: 2024/04/13 18:39:02 by gprada-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,42 @@ t_game	*init_game()
 	return (NULL);
 }
 
+void	parse_color(t_game *game, char *line)
+{
+	int		i;
+	int		r;
+	int		g;
+	int		b;
+
+	i = 0;
+	while (line[i] != ',')
+		i++;
+	r = ft_atoi(line + 1);
+	g = ft_atoi(line + i + 1);
+	b = ft_atoi(line + i + 1);
+}
+
+void	parse_texture(t_game *game, char *line)
+{
+	int		i;
+	char	*path;
+
+	i = 0;
+	while (line[i] != ' ')
+		i++;
+	path = ft_strdup(line + i);
+}
+
 int	parse_map(t_game *game, char *argv)
 {
 	int		i;
 	int		fd;
-
+	char	*line;
 	i = -1;
 	fd = open(argv, O_RDONLY);
-	while (get_next_line(fd, &line) > 0)
+	while (get_next_line(fd) > 0)
 	{
-		if (line[0] == 'R')
-			parse_resolution(game, line);
-		else if (line[0] == 'F' || line[0] == 'C')
+		if (line[0] == 'F' || line[0] == 'C')
 			parse_color(game, line);
 		else if (line[0] == 'N' || line[0] == 'S' || line[0] == 'E' || line[0] == 'W')
 			parse_texture(game, line);
@@ -48,10 +72,18 @@ int	parse_map(t_game *game, char *argv)
 		}
 		free(line);
 	}
+}
+
+void	parse_map(char *line)
+{
+	int		i;
+
+	i = -1;
+	while (line[++i])
 	{
-		if (argv[i] != ' ' || argv[i] != '1' || argv[i] != '0'
-			|| argv[i] != 'N' || argv[i] != 'S'
-			|| argv[i] != 'E' || argv[i] != 'W')
+		if (line[i] != ' ' || line[i] != '1' || line[i] != '0'
+			|| line[i] != 'N' || line[i] != 'S'
+			|| line[i] != 'E' || line[i] != 'W')
 		{
 			ft_putstr_fd("Error\nInvalid map\n", 2);
 			return (FAILURE);
@@ -60,13 +92,35 @@ int	parse_map(t_game *game, char *argv)
 	return (SUCCESS);
 }
 
+int	check_path(char *path)
+{
+	int		i;
+
+	i = ft_strlen(path);
+	if (path[i - 1] == 'b' && path[i - 2] == 'u' && path[i - 3] == 'c' && path[i - 4] == '.')
+		return (SUCCESS);
+	return (FAILURE);
+}
+
 int main(int argc, char **argv)
 {
 	t_game *game;
 
 	if (argc == 2)
-		if (parse_map(argv[1]) == FAILURE)
+	{
+		if(check_path(argv[1]) == SUCCESS)
+		{
+			game = init_game();
+			if (parse_map(game, argv[1]) == FAILURE)
+				return (FAILURE);
+
+		}
+		else
+		{
+			ft_putstr_fd("Error\nInvalid file\n", 2);
 			return (FAILURE);
+		}
+	}
 	else
 	{
 		ft_putstr_fd("Error\nInvalid number of arguments\n", 2);
